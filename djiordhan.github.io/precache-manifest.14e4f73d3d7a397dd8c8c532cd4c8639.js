fa69d1ee14740e124777575741389b52225f131f
Jordan Diamante <diamantejordan@gmail.com>
2018-07-29 02:00:38 +0800

    change github page to vuejs

---
+++ djiordhan.github.io/precache-manifest.14e4f73d3d7a397dd8c8c532cd4c8639.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "470fc51a4b02c26fa969",
+    "url": "/js/chunk-vendors.470fc51a.js"
+  },
+  {
+    "revision": "474acb5eb47d7a4d997b",
+    "url": "/js/app.474acb5e.js"
+  },
+  {
+    "revision": "8559b8fc82bdd0de308f",
+    "url": "/js/about.8559b8fc.js"
+  },
+  {
+    "revision": "12212a5f80bab4afd6c8b666494ae9e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "474acb5eb47d7a4d997b",
+    "url": "/css/app.68002cc9.css"
+  }
+];
